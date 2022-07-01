ec3083e7472ea038d29bb691321609aa1bbe141b
AturiheihiBlendon <2020blendon@gmail.com>
2022-07-01 04:35:57 +0300

    Infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == '__main__':
     import sys
     add = 0
-    for i in range(1, len(sys.argv)):
+    for i in range(len(sys.argv) - 1):
         add += int(sys.argv[i])
         print('{:d}'.format(add))
