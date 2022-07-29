f72983bb7a50ed8294fa48266abdf78ff396e9fc
AturiheihiBlendon <2020blendon@gmail.com>
2022-07-29 14:32:55 +0300

    Task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/python3
+
+
+"""
+A module that has a function to print
+a square with character "#"
+"""
+
+
+def print_square(size):
+    """
+    Function to print a square with
+    character "#"
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)
