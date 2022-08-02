59fc41b92032c9c5f0f84cc74f126854a9089043
AturiheihiBlendon <2020blendon@gmail.com>
2022-08-02 18:57:04 +0300

    Task 0 and 1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+
+
+"""
+A module that has a function to read a file
+"""
+
+
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+
+
+"""
+A module that has a function to write
+to a file
+"""
+
+
 def write_file(filename="", text=""):
     """Write a string to a UTF8 text file."""
     with open(filename, "w", encoding="utf-8") as f:
