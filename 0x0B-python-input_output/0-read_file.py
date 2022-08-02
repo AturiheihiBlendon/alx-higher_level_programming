177c421853c4c8ac7c055d93d615e7e762649bf2
AturiheihiBlendon <2020blendon@gmail.com>
2022-08-02 19:00:20 +0300

    Task 0 and 1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ A module that has a function to read a file
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
